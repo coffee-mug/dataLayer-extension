22f27c665a18e0fa95bd630b176f69bc6d6aaeeb
Lucas Kostka <lucas@fifty-five.com>
2019-09-25 12:45:11 +0200

    Reset everything

--- src/lib/actions/datalayerEvent.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function(settings) {
-  // TODO Perform some action.
-};
--- src/lib/dataElements/datalayer.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function(settings) {
-  // TODO Return the data element value.
-};
